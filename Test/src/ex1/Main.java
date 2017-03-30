82d154a546b2dc860af8df1aea7980bc3a99fae3
Sergey Ivanov <ivsaemail@gmail.com>
2017-03-30 12:26:15 +0300

    add comment

--- Test/src/ex1/Main.java
+++ Test/src/ex1/Main.java
@@ -1,10 +1,10 @@
 package ex1;
 
 /**
- * Created by Dendi on 3/30/2017.
+ * Created by Dendi on 3/30/2006.
  */
 public class Main {
     public static void main(String[] args) {
-        new HelloWorld();
+        new HelloWorld(); // sout "hello, World"
     }
 }
